5b5f872cde931eae27210b2600fea3818214c918
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2023-03-04 23:24:36 +0200

    hw5

---
+++ scripts/task3.js
@@ -0,0 +1,13 @@
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+const John = 100
+const Ann = 160
+const Pete = 130
+
+for (let summ in salaries) {
+    sum = parseFloat(John)+parseFloat(Ann)+parseFloat(Pete)
+}
+console.log(sum)
